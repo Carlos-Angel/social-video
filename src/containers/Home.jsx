1926c43ac70dfb450c6619d0d43259f1aeafc0e2
Charly_Angel <c.a.a.a.503@gmail.com>
2021-03-07 08:40:02 -0600

    feat(react-roter-dom): implementando el sistema de rutas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-export default function App() {
+export default function Home() {
   const initialState = useInitialState(API);
   return (
     <div className='App'>
