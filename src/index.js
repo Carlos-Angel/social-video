769362e69e7111673c87d6c69a9e2b595eab2d1b
Charly_Angel <c.a.a.a.503@gmail.com>
2021-03-09 07:45:20 -0600

    feat: implementando compose para debugear nuestra aplicación con redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import App from './routes/App';
 import initialState from './initialState';
@@ -9,7 +9,8 @@ import reducer from './reducers';
 
 import './assets/styles/App.scss';
 
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
