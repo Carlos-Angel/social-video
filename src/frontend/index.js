01c8e71519f5d2872a88ec13f8177045fba53b0a
Charly_Angel <c.a.a.a.503@gmail.com>
2021-03-18 19:34:26 -0600

    feat(react-router):abstrayendo las rutas creando un arreglo

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
 
 import App from './routes/App';
 import initialState from './initialState';
@@ -9,12 +11,15 @@ import reducer from './reducers';
 
 import './assets/styles/App.scss';
 
+const history = createBrowserHistory();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
