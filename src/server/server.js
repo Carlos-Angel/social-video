8c97d81aa6bc7510cee474df92f2249ae567babc
Charly_Angel <c.a.a.a.503@gmail.com>
2021-07-23 22:27:37 -0500

    feat: configuración de vendors en webpack

--- src/server/server.js
+++ src/server/server.js
@@ -47,8 +47,9 @@ if (ENV === 'development') {
 }
 
 const setResponse = (html, preloadedState, manifest) => {
-  const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+  const mainStyles = manifest ? manifest['vendors.css'] : 'assets/app.css';
   const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+  const venderBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
   return `
   <!DOCTYPE html>
   <html lang="en">
@@ -68,6 +69,7 @@ const setResponse = (html, preloadedState, manifest) => {
     )}
     </script>
     <script src="${mainBuild}" type="text/javascript" ></script>
+    <script src="${venderBuild}" type="text/javascript" ></script>
   </body>
   </html>
   `;
