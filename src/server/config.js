8c855427ba3228d6d1f1995ec0c33e0d37e0cb65
Charly_Angel <c.a.a.a.503@gmail.com>
2021-07-26 22:46:23 -0500

    fix: agregar session secret

--- src/server/config.js
+++ src/server/config.js
@@ -4,6 +4,7 @@ const config = {
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT,
   apiUrl: process.env.API_URL,
+  sessionSecret: process.env.SESSION_SECRET,
   apiKeyToken: process.env.API_KEY_TOKEN,
 };
 
