789aba0d3a87337e627237640e82aa4e3a57534c
Charly_Angel <c.a.a.a.503@gmail.com>
2021-07-27 21:25:05 -0500

    test: prueba de imagen de gravatar

---
+++ src/frontend/__test__/utils/gravatar.test.js
@@ -0,0 +1,9 @@
+import gravatar from '../../utils/gravatar';
+
+test('gravatar function test', () => {
+  const email = 'oscar@arepa.dev';
+  const gravatarUrl =
+    'https://gravatar.com/avatar/4b84ab1122c7ee522710c5ead5525839';
+
+  expect(gravatarUrl).toEqual(gravatar(email));
+});
